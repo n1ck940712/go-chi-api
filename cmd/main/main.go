2166bc89438e24411935004560ada9e993639cb9
Nick Hoo <cheehow.hoo@r4pid.com>
2023-02-20 20:19:13 +0800

    function to init container base on type

---
+++ cmd/main/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"go-chi-api/internal/build/api"
+	"os"
+)
+
+func main() {
+	switch getServerType() {
+	case "api":
+		api.Run(getServerIdentifier(), getPort())
+	default:
+		panic("unsupported server type: \"" + getServerType() + "\"")
+	}
+}
+
+func getServerType() string {
+	return os.Getenv("SERVER_TYPE")
+}
+
+func getServerIdentifier() string {
+	return os.Getenv("SERVER_IDENTIFIER")
+}
+
+func getPort() string {
+	return os.Getenv("SERVER_PORT")
+}
