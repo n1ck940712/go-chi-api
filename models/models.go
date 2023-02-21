b7af9cfc70604da2fe28d489ac7ba88a4b917a5e
Nick Hoo <n1ck940712@gmail.com>
2023-02-21 22:39:50 +0800

    update ItemTable

--- models/models.go
+++ models/models.go
@@ -2,16 +2,12 @@ package models
 
 import (
 	"time"
-
-	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type ItemTable struct {
-	gorm.Model
-	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Column1   string    `json:"column1" gorm:"text;not null;default:null"`
-	Column2   string    `json:"column2" gorm:"text;not null;default:null"`
+	ID        int64
+	Name      string `gorm:"text;not null;default:null"`
+	Attribute string `gorm:"text;not null;default:null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
