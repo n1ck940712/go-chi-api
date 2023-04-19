5f02dc3140923c30f41709b36cd17941047ce858
Nick Hoo <n1ck940712@gmail.com>
2023-04-19 17:11:54 +0800

    add user obj to request context during token validation

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -1,17 +1,20 @@
 package middlewares
 
 import (
+	"context"
 	"go-chi-api/internal/auth"
 	"net/http"
 )
 
 func BaseAuthentication(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		if err := auth.TokenValid(r); err != nil {
+		user, err := auth.TokenValid(r)
+		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-		h.ServeHTTP(w, r)
+		ctx := context.WithValue(r.Context(), "user", user)
+		h.ServeHTTP(w, r.WithContext(ctx))
 	}
 	return http.HandlerFunc(fn)
 }
